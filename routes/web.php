7f041d43b1b0ef567871167f5d268ded219642d7
Lin Yu Lun <wuyahayaa01@gmail.com>
2020-10-30 13:06:34 +0800

    編輯新增任務route

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,22 @@ Route::group(['middleware' => 'web'], function () {
      */
     Route::post('/task', function (Request $request)
     {
-        //
+        //驗證輸入
+        $validator = Validator::make($request->all(),
+            ['name' => 'required|max:255',]);
+
+        if ($validator->fails())
+        {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+        // 建立該任務...
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+        return redirect('/');
+        //新增任務存入DB的程式碼(see next page)
     });
 
     /**
